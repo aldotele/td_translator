53650c3ded96e4b827dfc0b10bbe01d3cfb78ab5
aldotele <aldo.telese@hotmail.it>
2021-07-12 22:19:19 +0200

    prevent redirecting to result by using javascript

--- translator/views.py
+++ translator/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from googletrans import Translator
+import json
 
 
 # Create your views here.
@@ -11,17 +12,29 @@ def home(request, slug = "it"):
 
 
 def translate(request):
+    print(request)
     tr = Translator()
     context = {}
-    if request.method == 'POST':
+    data = json.loads(request.body)
+    word = data['tr_content']
+    dest_language = data['slug']
+    out = tr.translate(word, dest=dest_language).text
+    context['translated'] = out
+    context['slug'] = dest_language
+    print(context)
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
+    #if request.method == 'POST':
         #print(request.POST)
-        word = request.POST.get('word', '')
-        dest_language = request.POST.get('slug', '')
-        #print(dest_language)
-        out = tr.translate(word, dest=dest_language).text
-        context['translated'] = out
-        context['slug'] = dest_language
-    return render(request, 'result.html', context)
+        # word = request.POST.get('tr_content', '')
+        # dest_language = request.POST.get('slug', '')
+        # print(word)
+        # print(dest_language)
+        # out = tr.translate(word, dest=dest_language).text
+        # context['translated'] = out
+        # context['slug'] = dest_language
+    #return render(request, 'result.html', context)
 
 
 if __name__ == '__main__':
